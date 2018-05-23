7e1832a6622b8298704951a57632f03a0205740f
Kareliavs <karelia.vilca@ucsp.edu.pe>
2018-05-23 16:06:39 -0500

    Lista de historias

--- app/src/main/java/com/historial/medistory/HistorialFragment.java
+++ app/src/main/java/com/historial/medistory/HistorialFragment.java
@@ -1,12 +1,21 @@
 package com.historial.medistory;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,11 +26,14 @@ import android.view.ViewGroup;
  * Use the {@link HistorialFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class HistorialFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ListView list;
+    private String[] historias ={"Curación de caries","Revisión","Retiro de muela"};
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,13 +70,52 @@ public class HistorialFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historial, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_historial,container,false);
+        ArrayList<String> data= new ArrayList<>();
+        data.add("Retiro de muela");
+        data.add("Caries");
+        data.add("Limpieza");
+        data.add("Caries");
+        data.add("Limpieza");
+        data.add("Retiro de muela");
+        data.add("Caries");
+        data.add("Limpieza");
+        data.add("Retiro de muela");
+        data.add("Caries");
+        data.add("Limpieza");
+        data.add("Caries");
+        data.add("Limpieza");
+        data.add("Caries");
+        data.add("Limpieza");
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.fila_lista,
+                R.id.nombre_fila_lista,
+
+                data
+        );
+        ListView lvData = (ListView) view.findViewById(R.id.lista);
+        lvData.setAdapter(adapter1);
+        lvData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getActivity().getApplicationContext(),String.valueOf(position),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext() , DetalleActivity.class);
+                getContext().startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
